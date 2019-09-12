a643ce19c12e7cacdefff582d08bf1bd28c6ba86
Alek2750 <alek2750@gmail.com>
2019-09-12 22:30:35 +0200

    maps & battery done

---
+++ Xamarin_Assignment2/Xamarin_Assignment2/Xamarin_Assignment2/Pages/MapPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Xamarin_Assignment2.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MapPage : ContentPage
+    {
+        public MapPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void NavigateButton_OnClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new MainPage());
+        }
+
+        private async void ButtonOpenCoords_Clicked(object sender, EventArgs e)
+        {
+            if (!double.TryParse(EntryLatitude.Text, out double lat))
+                return;
+
+            if (!double.TryParse(EntryLongitude.Text, out double lng))
+                return;
+
+            await Map.OpenAsync(lat, lng, new MapLaunchOptions
+            {
+                Name = EntryName.Text,
+                NavigationMode = NavigationMode.None
+            });
+
+            
+        }
+
+    }
+}
