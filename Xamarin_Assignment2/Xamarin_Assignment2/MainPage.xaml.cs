9261e92c2678cd3e3bb4ceccda8971fba060a14a
Alek2750 <alek2750@gmail.com>
2019-09-16 14:14:55 +0200

    4 things done

---
+++ Xamarin_Assignment2/Xamarin_Assignment2/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Essentials;
+using Xamarin_Assignment2.Pages;
+
+namespace Xamarin_Assignment2
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            
+        }
+        private async void NavigateButton_OnClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ShowBattery());
+        }
+        private async void MapButton_OnClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new MapPage());
+        }
+
+        private async void ShareButton_OnClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new SharePage());
+        }
+
+        private async void AccelButton_OnClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new AccelPage());
+        }
+    }
+}
---
+++ Xamarin_Assignment2/Xamarin_Assignment2/Pages/AccelPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Xamarin_Assignment2.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AccelPage : ContentPage
+    {
+        public AccelPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonStart_Clicked(object sender, EventArgs e)
+        {
+            if (Accelerometer.IsMonitoring)
+                return;
+
+            Accelerometer.ReadingChanged += Accelerometer_ReadingChanged;
+            Accelerometer.Start(SensorSpeed.UI);
+        }
+
+        private void Accelerometer_ReadingChanged(object sender, AccelerometerChangedEventArgs e)
+        {
+            LabelX.Text = e.Reading.Acceleration.X.ToString();
+            LabelY.Text = e.Reading.Acceleration.Y.ToString();
+            LabelZ.Text = e.Reading.Acceleration.Z.ToString();
+
+            
+        }
+
+        private void ButtonStop_Clicked(object sender, EventArgs e)
+        {
+            if (!Accelerometer.IsMonitoring)
+                return;
+
+            Accelerometer.ReadingChanged -= Accelerometer_ReadingChanged;
+            Accelerometer.Stop();
+        }
+    }
+}
